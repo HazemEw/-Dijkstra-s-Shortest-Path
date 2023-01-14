23d728f8edeac4e87743709697a9117d4422e8c7
ewidathazem <121430364+ewidathazem@users.noreply.github.com>
2023-01-14 20:44:32 +0200

    Delete Compression System directory

--- Compression System/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.hufmman_hazem {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens Hazem_Huffman to javafx.fxml;
-    exports Hazem_Huffman;
-}
